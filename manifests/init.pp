6f484d975dc31aacd43851d51761f809a1c6e5ab
Matt Heath <matt@mattheath.com>
2013-07-23 18:29:48 +0100

    Ensure directory structure is created

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,15 @@
 class graphite {
   include graphite::carbon
   include graphite::whisper
+
+  # Create directory structure
+  file { [
+    $graphite::config::basedir,
+    $graphite::config::libdir,
+    $graphite::config::confdir,
+    $graphite::config::bindir,
+  ]:
+    ensure  => directory,
+  }
+
 }

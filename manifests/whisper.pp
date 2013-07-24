83d87648138206b5cf057d4e2cc92ce4da0a80ee
Matt Heath <matt@mattheath.com>
2013-07-24 16:47:36 +0100

    Should probably ensure python is installed prior to installing whisper

--- manifests/whisper.pp
+++ manifests/whisper.pp
@@ -1,16 +1,19 @@
 class graphite::whisper {
+
   include boxen::config
   include graphite::config
+  include python
 
   repository { "${boxen::config::cachedir}/whisper":
     source    => 'graphite-project/whisper',
     provider  => 'git'
   }
 
-  exec { 'install whisper':
+  exec { 'install-whisper':
     command => "cd ${graphite::config::builddir}/whisper && python setup.py install --prefix=${graphite::config::basedir} --install-lib=${graphite::config::libdir} --install-scripts=${graphite::config::bindir}",
     creates => "${graphite::config::bindir}/whisper-info.py",
     require => [
+      Class['python'],
       Repository["${boxen::config::cachedir}/whisper"],
       File[$graphite::config::bindir],
       File[$graphite::config::libdir],
@@ -21,6 +24,6 @@ class graphite::whisper {
   file { "${graphite::config::bindir}/whisper.py":
     target  => "${graphite::config::libdir}/whisper.py",
     ensure  => link,
-    require => Exec['install whisper'],
+    require => Exec['install-whisper'],
   }
 }

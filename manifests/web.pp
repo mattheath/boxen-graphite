bce09ed2501bba2f1a34a24bdbbddabd132ef61f
Matt Heath <matt@mattheath.com>
2013-07-24 21:15:26 +0100

    Unchain Django 1.3 onto our boxen

--- manifests/web.pp
+++ manifests/web.pp
@@ -50,4 +50,31 @@ class graphite::web {
     ensure => '3.4',
   }
 
+  # A sprinkling of Django
+
+  $django_version = '1.3'
+
+  repository { "${boxen::config::cachedir}/django":
+    source    => 'django/django',
+    provider  => 'git'
+  }
+
+  exec { "ensure-django-version-${django_version}":
+    command => "git fetch && git reset --hard ${django_version}",
+    cwd     => "${boxen::config::cachedir}/django",
+    unless  => "git describe --tags --exact-match `git rev-parse HEAD` | grep ${django_version}",
+    require => Repository["${boxen::config::cachedir}/django"],
+    notify  => Exec['install-django'],
+  }
+
+  exec { 'install-django':
+    command   => "cd ${graphite::config::builddir}/django && python setup.py install",
+    #creates   => "${graphite::config::bindir}/carbon-cache.py",
+    require   => [
+      Class['python'],
+      File[$graphite::config::bindir],
+      File[$graphite::config::libdir],
+    ],
+  }
+
 }

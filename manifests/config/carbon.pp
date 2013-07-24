681d95f2e5c0e534e9883db0f077f425b4d4395b
Matt Heath <matt@mattheath.com>
2013-07-24 18:26:47 +0100

    Define path for carbon executable

--- manifests/config/carbon.pp
+++ manifests/config/carbon.pp
@@ -2,6 +2,10 @@
 
 class graphite::config::carbon {
 
+  include graphite::config
+
+  $executable = "${graphite::config::bindir}/carbon-cache.py"
+
   $line_receiver_port   = '12003'
   $udp_receiver_port    = '12003'
   $pickle_receiver_port = '12004'
